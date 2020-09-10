2d0a164b94f0ea5cb4141e86d53c14ce5b73bd97
binish <binishk@bgsu.edu>
2020-09-10 00:53:21 -0400

    Add nginx

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 5000
-
+const cors = require('cors');
+app.use(cors());
+app.options('*', cors());
 app.get('/', (req, res) => res.send("Home Page"))
 
 app.listen(port, () => console.log(`Express server started at http://localhost:${port}`))
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
+
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
 function App() {
+  const [data, setData] = useState("what is this")
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: BACKEND_URL,
+    })
+    .then(response => setData(response.data))
+    .catch(error => console.log(error))
+  })
   return (
     <div className="App">
-    <h1> what is this </h1>
+    <h1> {data} </h1>
     </div>
   );
 }
